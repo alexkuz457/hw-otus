3a253171f498cfd175cbe9c05bb0eff337056d37
alexkuz457 <alexkuz@bk.ru>
2022-12-04 19:39:38 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,9 @@
 package main
 
+import "golang.org/x/example/stringutil"
+
 func main() {
-	// Place your code here.
+	originalString := "Hello, Otus!"
+	reverseString := stringutil.Reverse(originalString)
+	print(reverseString)
 }
