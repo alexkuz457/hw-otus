5f3a00318ffcc2c57965e6d7b6eba6d3c6a69bd5
alexkuz457 <alexkuz@bk.ru>
2023-10-24 17:42:50 +0300

    HW1 v3

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -3,9 +3,7 @@ package main
 import "golang.org/x/example/hello/reverse"
 
 func main() {
-
 	originalString := "Hello, Otus!"
 	reverseString := reverse.String(originalString)
 	print(reverseString)
-
 }
